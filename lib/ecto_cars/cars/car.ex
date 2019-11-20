be7a539951ee32481fecec3cbe13657fa462cec3
Avidor Turkewitz <aviturkewitz@gmail.com>
2019-11-20 17:35:15 -0500

    add has_named_binding?

--- lib/ecto_cars/cars/car.ex
+++ lib/ecto_cars/cars/car.ex
@@ -1,7 +1,7 @@
 defmodule EctoCars.Car do
   use Ecto.Schema
   import Ecto.Changeset, only: [cast: 3, validate_required: 2]
-  import Ecto.Query, only: [where: 3, join: 5]
+  import Ecto.Query, only: [where: 3, join: 5, has_named_binding?: 2]
 
   alias EctoCars.{Car, Engine, Specification, Transmission}
 
@@ -25,15 +25,24 @@ defmodule EctoCars.Car do
 
   def with_transmission(query \\ Car, type) do
     query
-    |> join(:left, [c], s in Specification, as: :specifications, on: c.specification_id == s.id)
+    |> join_specifications()
     |> join(:left, [c, specifications: s], t in Transmission, as: :transmissions, on: s.transmission_id == t.id)
     |> where([c, transmissions: t], t.type == ^type)
   end
 
   def with_engine_horse_power(query \\ Car, horse_power) do
     query
-    |> join(:left, [c], s in Specification, as: :specifications, on: c.specification_id == s.id)
+    |> join_specifications()
     |> join(:left, [c, specifications: s], e in Engine, as: :engines, on: s.engine_id == e.id)
     |> where([c, engines: e], e.horse_power > ^horse_power)
+  end
+
+  defp join_specifications(query) do
+    if has_named_binding?(query, :specifications) do
+      query
+    else
+      query
+      |> join(:left, [c], s in Specification, as: :specifications, on: c.specification_id == s.id)
+    end
   end
 end
