875e67cc246447d59bc6e0e825757ce7b16f6646
Avidor Turkewitz <aviturkewitz@gmail.com>
2019-11-20 16:39:28 -0500

    add transmission query

--- lib/ecto_cars/cars/car.ex
+++ lib/ecto_cars/cars/car.ex
@@ -3,7 +3,7 @@ defmodule EctoCars.Car do
   import Ecto.Changeset, only: [cast: 3, validate_required: 2]
   import Ecto.Query, only: [where: 3, join: 5]
 
-  alias EctoCars.{Car, Specification}
+  alias EctoCars.{Car, Specification, Transmission}
 
   schema "cars" do
     field(:color, :string)
@@ -21,5 +21,12 @@ defmodule EctoCars.Car do
   def with_color(color) do
     Car
     |> where([c], c.color == ^color)
+  end
+
+  def with_transmission(type) do
+    Car
+    |> join(:left, [c], s in Specification, on: c.specification_id == s.id)
+    |> join(:left, [c, s], t in Transmission, on: s.transmission_id == t.id)
+    |> where([c, s, t], t.type == ^type)
   end
 end
