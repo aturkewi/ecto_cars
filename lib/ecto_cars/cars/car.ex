8cb5898f5284c69ed7e437dce626c61c8dd05448
Avidor Turkewitz <aviturkewitz@gmail.com>
2019-11-20 16:28:23 -0500

    add with_colors

--- lib/ecto_cars/cars/car.ex
+++ lib/ecto_cars/cars/car.ex
@@ -1,8 +1,9 @@
 defmodule EctoCars.Car do
   use Ecto.Schema
   import Ecto.Changeset, only: [cast: 3, validate_required: 2]
+  import Ecto.Query, only: [where: 3, join: 5]
 
-  alias EctoCars.Specification
+  alias EctoCars.{Car, Specification}
 
   schema "cars" do
     field(:color, :string)
@@ -15,5 +16,10 @@ defmodule EctoCars.Car do
     car
     |> cast(attrs, [:color, :vin_number, :specification_id])
     |> validate_required([:color, :vin_number, :specification_id])
+  end
+
+  def with_color(color) do
+    Car
+    |> where([c], c.color == ^color)
   end
 end
